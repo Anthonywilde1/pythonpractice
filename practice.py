e520734da5a3fa5b5945d4305ff8ce019279bde9
Anthony Wilde <Anthonywilde117.com>
2021-02-04 19:01:53 +1100

    made a list of the alphabet to spell my  name with concatenation

--- practice.py
+++ practice.py
@@ -4,3 +4,8 @@ a, b = 0, 1
 while a < 1000:
     print(a)
     a, b = b, a+b
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+name = letters[0] + letters[13] + letters[19] + letters[7] + letters[14] + letters[13] + letters[-2]
+print(name)
