c3fa6001b728faf453c641dbfef37879ec9a7621
Anthony Wilde <Anthonywilde117.com>
2021-02-04 19:08:05 +1100

    a for loop from the docs

--- practice.py
+++ practice.py
@@ -9,3 +9,22 @@ while a < 1000:
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 name = letters[0] + letters[13] + letters[19] + letters[7] + letters[14] + letters[13] + letters[-2]
 print(name)
+
+x = int(input("Please enter an integer: "))
+# input requires user input seems like stating the variable required
+#outside the brackets, in this case int for integer.
+if x < 0:
+    x = 0
+    print('Negative changed to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
+
+
+# Measure some strings:
+words = ['cat', 'window', 'defenestrate', 'Wowowowowowowow']
+for w in words:
+    print(w, len(w))
